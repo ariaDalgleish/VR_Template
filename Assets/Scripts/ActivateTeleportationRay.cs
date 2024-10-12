1e143e3b8c7c7aca1b1fd54a18cc5cabffa1e46d
ariaDalgleish <dalgleisharia07@gmail.com>
2024-10-12 17:10:39 +1300

    firing bullet finish

--- Assets/Scripts/ActivateTeleportationRay.cs
+++ Assets/Scripts/ActivateTeleportationRay.cs
@@ -11,10 +11,13 @@ public class ActivateTeleportationRay : MonoBehaviour
     public InputActionProperty leftActivate;
     public InputActionProperty rightActivate;
 
+    public InputActionProperty leftCancel;
+    public InputActionProperty rightCancel;
+
     // Update is called once per frame
     void Update()
-    {
-        leftTeleportation.SetActive(leftActivate.action.ReadValue<float>() > 0.1f);
-        rightTeleportation.SetActive(rightActivate.action.ReadValue<float>() > 0.1f);
+    { // make sure grid vaule is  zero and trigger vaule  is above a certain threshold to enable the teleportation
+        leftTeleportation.SetActive(leftActivate.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f); 
+        rightTeleportation.SetActive(rightActivate.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
     }
 }
